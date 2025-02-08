fedaff0ce62021f0f414264f27c678e61eae499b
aubreyjcook <aubreyjcook@gmail.com>
2025-02-08 04:15:37 -0600

    update js demo:basic details

--- js/src/App.js
+++ js/src/App.js
@@ -5,18 +5,23 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React.JS demo</h1>
+        <p>This app is a demo containing code meant to demonstrate foundational React.JS features and usecases created by developer Aubrey Cook. This demo focuses on traditional JavaScript, in contrast to a parallel demo written in TypeScript.</p>
+        <span className="concept-row-container">
+          <div>
+            <a href><h2>Hooks</h2></a>
+            <h3>Description</h3>
+            <p>Hooks description.</p>
+            <ul>
+              <a href><li>Concept 1</li></a>
+              <a href><li>Concept 1</li></a>
+              <a href><li>Concept 1</li></a>
+            </ul>
+          </div>
+          {/* <div >
+            <h2>Concept 1</h2>
+          </div> */}
+        </span>
       </header>
     </div>
   );
